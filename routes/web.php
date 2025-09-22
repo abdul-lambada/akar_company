0413d5b7528d54dffea5d9e34bfb9bbd9c261dda
Abdul Kholik <akl859349@gmail.com>
2025-09-22 17:11:26 +0700

    fix(auth): add missing route name for login post endpoint

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
