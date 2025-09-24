eab66657f730266587fbdce2c63a7d7b913f0c2b
Abdul Kholik <akl859349@gmail.com>
2025-09-24 11:48:35 +0700

    setup template fe

--- resources/views/public/partials/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer-wrapper diagonal-shadow">
-    <div class="container">
-        <div class="copyright text-center">
-            &copy; {{ now()->year }} {{ config('app.name') }}. All rights reserved.
-        </div>
-    </div>
-</footer>
